0e28c15a7c8f07e27de5f9d55d38c960b244e649
ibby <125179334+waiting-for-ibby@users.noreply.github.com>
2026-05-11 15:41:42 -0400

    heyyyy

---
+++ hi.js
@@ -0,0 +1,10 @@
+window.onload = function () {
+    let faviconIndex = 0;
+    const favicon = document.getElementById('favicon');
+    const icons = ['blue.png', 'pink.png', 'orange.png']; // Array of icons
+
+    setInterval(function() {
+        favicon.href = icons[faviconIndex];
+        faviconIndex = (faviconIndex + 1) % icons.length; // Cycle through array
+    }, 1000); // 1000ms = 1 second
+};
